6588f5bf286314336a0604b573a32f7136d29ae5
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2016-04-08 14:12:35 +0300

    drivers: Add AT88 eeprom driver into source control.

---
+++ at88driver.h
@@ -0,0 +1,43 @@
+#ifndef AT88DRIVER_H
+#define AT88DRIVER_H
+
+#include <ecl/drivers/i2cdevice.h>
+
+class AT88Driver : public I2CDevice
+{
+	Q_OBJECT
+public:
+	enum Model {
+		SC0104C,
+		SC1616C,
+	};
+
+	explicit AT88Driver(QObject *parent = 0);
+	int init();
+	int getEepromModel();
+	int getEepromSize();
+	int getZoneCount();
+	int getPageSize();
+	int getZoneSize();
+	QByteArray readZone(int zone);
+	int writeZone(int zone, const QByteArray &ba);
+	int writePage(int zone, int page, const QByteArray &ba);
+
+protected:
+	QByteArray readConfigMem();
+	int selectUserZone(int zone);
+	int writeUserZone(int zone, quint16 off, const QByteArray &data);
+	QByteArray readUserZone(int zone);
+
+private:
+	quint64 atr;
+	quint16 fabCode;
+	quint16 mtz;
+	quint32 cmc;
+	quint64 lot;
+	uchar dcr;
+	QByteArray ident;
+
+};
+
+#endif // AT88DRIVER_H
