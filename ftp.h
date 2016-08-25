f5544518e170f7910ad3bc336c3b29bf0f8d82cf
Kerim Enes Yilmaz <kerimenes@bilkon-kontrol.com.tr>
2016-08-25 19:10:11 +0300

    net: Add ftp transfer support.
    Since these codes are depending on some Qt4-only classes like QFtp and
    QUrlInfo, we only build these codes if we are not using Qt5.

---
+++ ftp.h
@@ -0,0 +1,28 @@
+#ifndef FTP_H
+#define FTP_H
+
+#include <QFtp>
+
+class Ftp : public QFtp
+{
+	Q_OBJECT
+public:
+	Ftp();
+
+	void ftpConnect(QString address, QString username, QString pass);
+	QString ftpAddress(const QString address);
+	QString ftpUsername(const QString username);
+	QString ftpPass(const QString pass);
+	void direction(QString command, const QString path);
+	void direction(QString rename, QString oldname, QString newname);
+	void direction(QString command);
+	void getFile(QString path);
+	void putFile(QString filePath, QString location);
+	void ftpClose(void);
+
+public slots:
+	void stateChanged(void);
+	void stateCommand(void);
+};
+
+#endif // FTP_H
