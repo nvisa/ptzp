19dde322f608dbefb34157b609ad9b234c7b2c7d
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2018-07-04 12:52:26 +0300

    ptzp: transport: tcp: Implement missing functionality.

--- ptzptcptransport.cpp
+++ ptzptcptransport.cpp
@@ -3,8 +3,9 @@
 
 #include <QTcpSocket>
 
-PtzpTcpTransport::PtzpTcpTransport(QObject *parent)
-	: QObject(parent)
+PtzpTcpTransport::PtzpTcpTransport(LineProtocol proto, QObject *parent)
+	: QObject(parent),
+	PtzpTransport(proto)
 {
 	sock = NULL;
 }
@@ -15,10 +16,16 @@ int PtzpTcpTransport::connectTo(const QString &targetUri)
 	if (flds.size() == 1)
 		flds << "4002";
 	sock = new QTcpSocket();
-	connect(sock, SIGNAL(connected()), SIGNAL(connected()));
+	sock->connectToHost(flds[0], flds[1].toInt());
+	connect(sock, SIGNAL(connected()), SLOT(connected()));
 	connect(sock, SIGNAL(disconnected()), SLOT(clientDisconnected()));
 	connect(sock, SIGNAL(readyRead()), SLOT(dataReady()));
 	return 0;
+}
+
+int PtzpTcpTransport::send(const char *bytes, int len)
+{
+	return sock->write(bytes, len);
 }
 
 void PtzpTcpTransport::connected()
@@ -28,7 +35,7 @@ void PtzpTcpTransport::connected()
 
 void PtzpTcpTransport::dataReady()
 {
-
+	protocol->dataReady(sock->readAll());
 }
 
 void PtzpTcpTransport::clientDisconnected()
--- ptzptcptransport.h
+++ ptzptcptransport.h
@@ -11,14 +11,16 @@ class PtzpTcpTransport : public QObject, public PtzpTransport
 {
 	Q_OBJECT
 public:
-	explicit PtzpTcpTransport(QObject *parent = 0);
+	explicit PtzpTcpTransport(LineProtocol proto, QObject *parent = 0);
 
 	int connectTo(const QString &targetUri);
+	int send(const char *bytes, int len);
 
 protected slots:
 	void connected();
 	void dataReady();
 	void clientDisconnected();
+
 protected:
 	QTcpSocket *sock;
 };
