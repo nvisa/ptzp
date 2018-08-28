c7aa07c15fcc6f946b3bb17ce114f81972a613f6
ozogulf <furkan.ozogul@bilkon-kontrol.com.tr>
2018-08-28 15:57:33 +0300

    patternng: Preset, patrol added.Some pattern functions fixed.
    ptzcontrolinterface: Some methods added.
    
    ptzpatterninterface: Preset and patrol methods added.
    
    ptzpdriver: Preset, patrol and some pattern api's added.

--- ptzcontrolinterface.h
+++ ptzcontrolinterface.h
@@ -4,7 +4,18 @@
 class PtzControlInterface
 {
 public:
-	virtual void sendCommand(int c, int par1, int par2) = 0;
+	enum Commands {
+		C_PAN_LEFT,			//0
+		C_PAN_RIGHT,		//1
+		C_TILT_UP,			//2
+		C_TILT_DOWN,		//3
+		C_ZOOM_IN,			//4
+		C_ZOOM_OUT,			//5
+		C_ZOOM_STOP,		//6
+		C_PAN_TILT_STOP		//7
+	};
+	virtual void sendCommand(int c, float par1, int par2) = 0;
+	virtual void goToPosition(float p, float t, int z) = 0;
 };
 
 #endif // PTZCONTROLINTERFACE
--- ptzpatterninterface.h
+++ ptzpatterninterface.h
@@ -1,6 +1,8 @@
 #ifndef PTZPATTERNINTERFACE
 #define PTZPATTERNINTERFACE
 
+#include "QString"
+
 class PtzPatternInterface
 {
 public:
@@ -8,6 +10,14 @@ public:
 	virtual void commandUpdate(int pan, int tilt, int zoom, int c, int par1, int par2) = 0;
 	virtual int start(int pan, int tilt, int zoom) = 0;
 	virtual void stop(int pan, int tilt, int zoom) = 0;
+
+	virtual void addPreset(QString name, float panPos, float tiltPos, int zoomPos) = 0;
+	virtual void goToPreset(QString name) = 0;
+	virtual void deletePreset(QString name) = 0;
+	virtual void addPatrol(QString name, QString pSet, QString time) = 0;
+	virtual void runPatrol(QString name) = 0;
+	virtual void stopPatrol(QString name) = 0;
+	virtual void deletePatrol(QString name) = 0;
 };
 
 #endif // PTZPATTERNINTERFACE
