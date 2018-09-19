f5476c4eedf8bb7dd64bd6236a84ebc0d537ac21
Kerim Enes YILMAZ <kerimenes@bilkon-kontrol.com.tr>
2018-09-19 11:35:09 +0300

    Ecl:Drivers: New Preset, Patrol method singleton class added.

---
+++ presetng.h
@@ -0,0 +1,22 @@
+#ifndef PRESETNG_H
+#define PRESETNG_H
+
+#include <QHash>
+#include <QMutex>
+
+class PresetNg
+{
+public:
+	static PresetNg *getInstance();
+	int save();
+	int load();
+	int addPreset(const QString &name, float panPos, float tiltPos, int zoomPos);
+	QStringList getPreset(const QString &name);
+	int deletePreset(const QString &name);
+private:
+	PresetNg();
+	QMutex mutex;
+	QHash<QString, QString> presets;
+};
+
+#endif // PRESETNG_H
