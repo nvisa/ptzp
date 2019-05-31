b888c6231246da56671c0a3c8a1f7a6c33bab2aa
Furkan OZOGUL <furkan.ozogul@bilkon-kontrol.com.tr>
2019-05-31 18:06:49 +0300

    ptzp: flirdriver: new cam driver added.

--- flirdriver.cpp
+++ flirdriver.cpp
@@ -1,5 +1,39 @@
 #include "flirdriver.h"
 
+#include "flirmodulehead.h"
+#include "flirpthead.h"
+
 FlirDriver::FlirDriver()
 {
+	headModule = new FlirModuleHead;
+	headDome = new FlirPTHead;
+	defaultPTHead = headDome;
+	defaultModuleHead = headModule;
+}
+
+FlirDriver::~FlirDriver()
+{
+
+}
+
+PtzpHead *FlirDriver::getHead(int index)
+{
+	if(index == 0)
+		return headModule;
+	else if (index == 1)
+		return headDome;
+	return NULL;
+}
+
+int FlirDriver::setTarget(const QString &targetUri)
+{
+	QStringList fields = targetUri.split(";");
+	headModule->connect(fields[0]);
+	headDome->connectHTTP(fields[1]);
+	return 0;
+}
+
+void FlirDriver::timeout()
+{
+	PtzpDriver::timeout();
 }
--- flirdriver.h
+++ flirdriver.h
@@ -1,10 +1,27 @@
 #ifndef FLIRDRIVER_H
 #define FLIRDRIVER_H
 
-class FlirDriver
+#include <ecl/ptzp/ptzpdriver.h>
+
+class FlirModuleHead;
+class FlirPTHead;
+
+class FlirDriver : public PtzpDriver
 {
+	Q_OBJECT
 public:
 	FlirDriver();
+	~FlirDriver();
+
+	PtzpHead * getHead(int index);
+	int setTarget(const QString &targetUri);
+
+protected slots:
+	void timeout();
+
+protected:
+	FlirModuleHead *headModule;
+	FlirPTHead *headDome;
 };
 
 #endif // FLIRDRIVER_H
